e08f7c3adbc5b8a66a25cb5dc54d98fe27eb0071
Salman-Ali05 <sallukhan0805gmail.com>
2023-10-11 10:55:45 +0200

    crud finish on exercice

--- GoMuscu/Exercice/views.py
+++ GoMuscu/Exercice/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404
+from rest_framework.generics import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
@@ -8,37 +8,31 @@ from .models import ExerciceModel
 
 class ExerciceViewSet(viewsets.ModelViewSet):
     queryset = ExerciceModel.objects.all()
-
     serializer_class = ExerciceSerializer
-
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-
         self.perform_create(serializer)
-        
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
-    def retreive(self, request, *args, **kwargs):
-        exercice_id = kwargs.get['pk'] # pk = primary key
+    def retrieve(self, request, *args, **kwargs):
+        exercice_id = self.kwargs.get('pk')  # pk = primary key
         exercice = get_object_or_404(ExerciceModel, pk=exercice_id)
-
         serializer = self.get_serializer(exercice)
-        return Response(serializer.data, status=status.HTTP_202_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     def partial_update(self, request, *args, **kwargs):
-        exercice_id = kwargs.get['pk'] # pk = primary key
+        exercice_id = self.kwargs.get('pk')  # pk = primary key
         exercice = get_object_or_404(ExerciceModel, pk=exercice_id)
-        
         serializer = self.get_serializer(exercice, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     def destroy(self, request, *args, **kwargs):
-        exercice_id = kwargs.get['pk'] # pk = primary key
+        exercice_id = kwargs['pk']
         exercice = get_object_or_404(ExerciceModel, pk=exercice_id)
 
         exercice.delete()
-        return Response(status=status.HTTP_202_OK_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
