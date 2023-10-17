744b039e7e0d3fa56a349a53daf6b87f6bb846ca
Salman-Ali05 <sallukhan0805gmail.com>
2023-10-17 12:27:46 +0200

    swagger doc done

--- GoMuscu/urls.py
+++ GoMuscu/urls.py
@@ -7,6 +7,18 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ESTIAM API",
+        default_version="v1"
+    ),
+    public=True,
+)
+
 
 router = routers.DefaultRouter()
 
@@ -18,5 +30,6 @@ urlpatterns = [
     path('', include(router.urls)),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
