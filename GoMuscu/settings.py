a46d054ed025a41e480c0db78e0e70890885cc08
Salman-Ali05 <sallukhan0805gmail.com>
2023-11-06 10:39:38 +0100

    fix url settings

--- GoMuscu/settings.py
+++ GoMuscu/settings.py
@@ -160,7 +160,7 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 CORS_ALLOWED_ORIGINS = [
-    "https://votre-site.com",
+    "http://localhost:4200",
     "http://localhost:8000",  # Par exemple, pour un développement local
     "http://127.0.0.1:9000",  # Une autre origine de développement local
 ]
